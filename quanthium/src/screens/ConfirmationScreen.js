430dbe636f729133a7f906baab7794bf88a39226
samliba <slibaire@outlook.com>
2022-07-15 11:32:23 -0400

    connected pages and installed necessary packages

---
+++ quanthium/src/screens/ConfirmationScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const ConfirmationScreen = () => {
+    return <Text style={{fontSize: 48}}>ConfirmationScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ConfirmationScreen;
---
+++ quanthium/src/screens/LendScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const LendScreen = () => {
+    return <Text style={{fontSize: 48}}>LendScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default LendScreen;
